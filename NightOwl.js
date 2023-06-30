4ec6b72bf12f8836936662ffb756f94342058269
BlackHFM <36536250+BlackHFM@users.noreply.github.com>
2023-06-30 04:04:50 +0330

    finifhed

--- NightOwl.js
+++ NightOwl.js
@@ -1,17 +1,31 @@
-
-
+let warnNullApiKey = document.getElementById("warnNullApiKey");
+let warnNoResult = document.getElementById("warnNoResult");
+let mainCard = document.getElementById("mainCard");
+let similarMoviesCard = document.getElementById("similarMoviesCard");
+mainCard.style.display = "none";
+warnNoResult.style.display = "none";
+warnNullApiKey.style.display = "none";
+similarMoviesCard.style.display = "none";
 
 const element = document.getElementById("search");
 element.addEventListener("click", searchMovie);
 
 function searchMovie() {
-    let apiKey = document.getElementById('apiKey').value;
+    // let apiKey = document.getElementById('apiKey').value;
     let movieName = document.getElementById('searchIMDB').value;
-    // let apiKey = 'k_izu1224f';
+    let apiKey = 'k_dic4z88a';
+    verifyApiKey(apiKey);
     // let movieName = 'interstellar';
     let searchMovieUrl = 'https://imdb-api.com/en/API/SearchMovie/' + apiKey + '/' + movieName;
     let titleUrl = 'https://imdb-api.com/en/API/Title/' + apiKey + '/';
-    getMovie(searchMovieUrl, titleUrl)
+    getMovie(searchMovieUrl, titleUrl);
+}
+
+function verifyApiKey (apiKey) {
+    if (apiKey.length < 1){
+        warnNullApiKey.style.display = "block";
+    }
+    else warnNullApiKey.style.display = "none";
 }
 
 
@@ -19,11 +33,28 @@ function getMovie(searchMovieUrl, titleUrl) {
     fetch(searchMovieUrl)
         .then((response) => response.json())
         .then((movieList) => {
-            if (movieList.results !== null) {
-                let movie = movieList.results[0];
-                console.log('movie', movie)
-                getInfo(titleUrl + movie.id);
+            console.log("movieList", movieList);
+            if (movieList.errorMessage.length < 1){
+                if (movieList.results.length !== 0) {
+                    mainCard.style.display = "flex";
+                    similarMoviesCard.style.display = "block";
+                    warnNoResult.style.display = "none";
+                    let movie = movieList.results[0];
+                    console.log('movie', movie)
+                    getInfo(titleUrl + movie.id);
+                }
+                else {
+                    mainCard.style.display = "none";
+                    similarMoviesCard.style.display = "none";
+                    warnNoResult.style.display = "block";
+                }
             }
+            else {
+                mainCard.style.display = "none";
+                similarMoviesCard.style.display = "none";
+                warnNoResult.style.display = "block";
+            }
+
         });
 }
 
@@ -41,19 +72,45 @@ function fillHtmlData(movie) {
     document.getElementById('movieName').innerHTML = movie.title;
     document.getElementById('movieYear').innerHTML = movie.year;
     document.getElementById('movieDuration').innerHTML = movie.runtimeStr;
+    document.getElementById('movieRating').innerHTML = movie.imDbRating;
     document.getElementById('moviePlot').innerHTML = movie.plot;
     document.getElementById('movieDirector').innerHTML = movie.directors;
     document.getElementById('movieWriter').innerHTML = movie.writers;
     document.getElementById('movieStars').innerHTML = movie.stars;
 
-    if (movie.directorList.size > 1){
+    if (movie.directorList.length > 1){
         document.getElementById('directorTitle').innerHTML = "Directors";
     }
     else document.getElementById('directorTitle').innerHTML = "Director";
-    if (movie.writerList.size > 1) {
+    if (movie.writerList.length > 1) {
         document.getElementById('writerTitle').innerHTML = "Writers";
     }
     else document.getElementById('writerTitle').innerHTML = "writer";
+
+    while (document.getElementById('genreList').hasChildNodes()){
+        document.getElementById('genreList').firstChild.remove();
+    }
+
+    for (let i = 0; i < movie.genreList.length; i++){
+        let element = document.createElement("div");
+        element.className = "genreItem";
+        element.innerHTML = movie.genreList[i].value;
+        document.getElementById('genreList').appendChild(element);
+    }
+
+    if (movie.awards.includes("Top rated movie")) {
+        document.getElementById("awards").style.display = "flex";
+        document.getElementById('topRated').innerHTML = movie.awards.split("|")[0];
+        document.getElementById('awardsList').innerHTML = movie.awards.split("|")[1];
+    }
+    else document.getElementById("awards").style.display = "none";
+
+    for (let i = 1; i < 5; i++) {
+        document.getElementById('similarImg' + i).src = movie.similars[i-1].image;
+        document.getElementById('similarScore' + i).innerHTML = movie.similars[i-1].imDbRating;
+        document.getElementById('similarTitle' + i).innerHTML = movie.similars[i-1].title;
+    }
+
 
 
 }
